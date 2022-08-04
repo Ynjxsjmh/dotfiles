03ee2b7efb16193aef1b2ab6b18f9d08e4e4e923
Ynjxsjmh <ynjxsjmh@gmail.com>
2022-08-04 14:57:39 +0800

    [xkeysnail] Remove multipurpose modmap

--- config/.config/xkeysnail/config.py
+++ config/.config/xkeysnail/config.py
@@ -58,31 +58,6 @@ define_conditional_modmap(re.compile(r'Emacs'), {
     Key.RIGHT_CTRL: Key.ESC,
 })
 
-# [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
-# released, and a modifier key when held down with another key. See Xcape,
-# Carabiner and caps2esc for ideas and concept.
-define_multipurpose_modmap(
-    # Enter is enter when pressed and released. Control when held down.
-    {Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
-
-    # Capslock is escape when pressed and released. Control when held down.
-    # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
-    # To use this example, you can't remap capslock with define_modmap.
-)
-
-# [Conditional multipurpose modmap] Multipurpose modmap in certain conditions,
-# such as for a particular device.
-define_conditional_multipurpose_modmap(lambda wm_class, device_name: device_name.startswith("Microsoft"), {
-   # Left shift is open paren when pressed and released.
-   # Left shift when held down.
-   Key.LEFT_SHIFT: [Key.KPLEFTPAREN, Key.LEFT_SHIFT],
-
-   # Right shift is close paren when pressed and released.
-   # Right shift when held down.
-   Key.RIGHT_SHIFT: [Key.KPRIGHTPAREN, Key.RIGHT_SHIFT]
-})
-
-
 # Keybindings for Firefox/Chrome
 define_keymap(re.compile("Firefox|Google-chrome"), {
     # Ctrl+Alt+j/k to switch next/previous tab
